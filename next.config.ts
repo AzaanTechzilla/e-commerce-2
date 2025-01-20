f533047a5a432265589d28d40e53e7cc6b517cfe
azan1234567890 <rajaazan918@gmail.com>
2025-01-20 12:23:15 +0500

    testing the vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY, // Include this to expose the variable to the client-side
+  },       // Example additional config option (optional)
 };
 
 export default nextConfig;
