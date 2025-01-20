3ee20e59cf57d3e172dc34dd9d77c3f817b27339
azan1234567890 <rajaazan918@gmail.com>
2025-01-20 13:37:49 +0500

    testing

--- src/app/component/banner/Banner.tsx
+++ src/app/component/banner/Banner.tsx
@@ -12,11 +12,11 @@ export default function Banner() {
         <div className="left-content  mx-3">
           <H1
             title="Step-up Your Stride"
-            className=" text-2xl mb-2 sm:text-4xl sm:mb-4 md:text-6xl md:mb-8 lg:text-8xl lg:mb-12 font-semibold"
+            className=" text-2xl text-black mb-2 sm:text-4xl sm:mb-4 md:text-6xl md:mb-8 lg:text-8xl lg:mb-12 font-semibold"
           />
           <Para
             title="Premium Athletic Footwear"
-            className="text-sm mb-2 sm:text-md sm:mb-2 md:text-lg md:mb-2 lg:text-2xl lg:mb-3"
+            className="text-sm mb-2 text-black sm:text-md sm:mb-2 md:text-lg md:mb-2 lg:text-2xl lg:mb-3"
           />
           <button className="uppercase flex self-start bg-black px-3 py-2 text-sm sm:text-md font-bold text-[rgb(254,199,79)] sm:px-4 sm:py-2 rounded-full">
             shop now
@@ -34,10 +34,10 @@ export default function Banner() {
           </div>
           <div className="basis-[30%] flex flex-col justify-between items-center">
             <div className="">
-              <H6 title="Marathon Master" className="text-lg font-bold mb-3" />
-              <Para title="$140.00" className="text-sm" />
+              <H6 title="Marathon Master" className="text-lg text-black font-bold mb-3" />
+              <Para title="$140.00" className="text-black text-sm" />
             </div>
-            <button className="uppercase border-[1px] border-black rounded-full px-2 py-1 text-sm ">Add to Cart</button>
+            <button className="uppercase border-[1px] text-black border-black rounded-full px-2 py-1 text-sm ">Add to Cart</button>
           </div>
         </div>
       </div>
