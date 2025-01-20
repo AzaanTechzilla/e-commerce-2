23fda4ca656dd9cff5de3dc4e69295c9c77e2b7c
azan1234567890 <rajaazan918@gmail.com>
2025-01-20 13:10:20 +0500

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY, // Include this to expose the variable to the client-side
-  },       // Example additional config option (optional)
+      // Example additional config option (optional)
 };
 
 export default nextConfig;
